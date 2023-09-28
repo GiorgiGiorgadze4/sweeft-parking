12e321ec35a6fceddd7993f9cfd1e6a01a77dbbe
kiu_giorgadze.giorgi <giorgadze.giorgi@kiu.edu.ge>
2023-09-28 18:00:03 +0400

    .env.example

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
 const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'db';
 const MYSQL_USER = process.env.MYSQL_USER || 'admin';
